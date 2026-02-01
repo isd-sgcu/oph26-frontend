9daf25dfa43380543c4a6ca69efbcddfdac9506c
BadLuckZ <thanagorn8802@gmail.com>
2026-02-02 00:25:12 +0700

    chore: questionaire page setup

--- src/routes/questionaire/index.tsx
+++ src/routes/questionaire/index.tsx
@@ -5,5 +5,14 @@ export const Route = createFileRoute('/questionaire/')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/questionaire"!</div>
+  return (
+    <div className="to-main-pink relative flex-1 overflow-hidden bg-linear-to-b from-[#ECECD2] to-10%">
+      {/* Content  */}
+      <div className={`relative z-10 flex w-full flex-col px-4 py-8`}>
+        <h1 className="text-center text-3xl font-bold text-white text-shadow-md">
+          Hello, Questionaire!
+        </h1>
+      </div>
+    </div>
+  )
 }
