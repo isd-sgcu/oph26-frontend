19ca090f3e16261625e099bd065a86c2628d523b
yiwahpsp <patsaporn.kaewkong@gmail.com>
2026-02-09 20:33:19 +0700

    feat: draft landing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -140,7 +140,6 @@ export default function Header() {
             {/* Sidebar Panel */}
             <div
               className={`fixed top-0 z-50 flex h-full min-h-screen w-full max-w-(--width-page) -translate-x-4 flex-col gap-4 overflow-auto bg-white px-4 py-8 shadow-lg ${isClosingSidebar ? 'animate-slide-out-left' : 'animate-slide-in-left'} `}
-              className={`fixed top-0 z-50 flex h-full min-h-screen w-full max-w-(--width-page) -translate-x-4 flex-col gap-4 overflow-auto bg-white px-4 py-8 shadow-lg ${isClosingSidebar ? 'animate-slide-out-left' : 'animate-slide-in-left'} `}
             >
               {/* Header */}
               <div className="flex items-center">
---
+++ src/components/landing/CountdownTimer.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+
+export default function CountdownTimer() {
+  const eventDate = new Date('2026-03-28T00:00:00')
+  const [timeLeft, setTimeLeft] = useState(getTimeLeft())
+
+  function getTimeLeft() {
+    const now = new Date()
+    const difference = eventDate.getTime() - now.getTime()
+
+    let timeLeft = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      }
+    }
+
+    return timeLeft
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(getTimeLeft())
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
+
+  return (
+    <div className="flex gap-4">
+      {
+        Object.entries(timeLeft).map(([unit, value]) => (
+          <div key={unit} className="flex flex-col items-center gap-1">
+            <span className="bg-main-beige shadow-sm p-2 rounded-lg font-bold text-main-pink text-4xl">{value.toString().padStart(2, '0')}</span>
+            <span className="font-semibold text-base">{unit.charAt(0).toUpperCase() + unit.slice(1)}</span>
+          </div>
+        ))
+      }
+    </div>
+  );
+}
