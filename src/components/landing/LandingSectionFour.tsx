c9786bcb8750147642e540a9ca9c48dcc443fbcb
Thanagorn Chaiyut <thanagorn8802@gmail.com>
2026-03-07 11:09:11 +0700

    fix: faq

--- src/components/landing/LandingSectionFour.tsx
+++ src/components/landing/LandingSectionFour.tsx
@@ -12,22 +12,40 @@ export default function LandingSectionFour() {
   const faqs = [
     {
       index: "0",
-      question: "What is CU Openhouse 2026?",
+      question: "งาน CU Open House 2026 จัดขึ้นวันที่เท่าไหร่?",
       answer:
-        "CU Openhouse 2026 is an event organized by Chulalongkorn University to showcase our academic programs, campus facilities, and student life to prospective students and their families.",
+        "28 - 29 มีนาคม 2026 เวลา 08:00 - 16:00 น.",
     },
     {
       index: "1",
-      question: "When and where will CU Openhouse 2026 take place?",
+      question: "งานจัดที่ไหน?",
       answer:
-        "CU Openhouse 2026 will be held on March 28-29, 2026, at Chulalongkorn University's main campus in Bangkok, Thailand.",
+        "งาน CU Open House 2026 มี 2 ส่วนด้วยกัน \n1. งานกิจกรรมส่วนกลาง @ จัดที่ศาลาพระเกี้ยว มีบูธจากทุกคณะในจุฬาฯ รวมถึงคณะแพทย์ด้วย! สามารถมาปรึกษาพี่ ๆ จาก 19 คณะ 1 สถาบัน และพิเศษ ในปีนี้มีพี่ ๆ จากหอพักนิสิต และชมรมต่าง ๆ มาพูดคุยด้วย อีกทั้งเรายังมีกิจกรรมตามล่าหาแสตมป์ ตาม journey ในจุฬา เพื่อแลกรับของรางวัล กิจกรรมปรึกษาเรื่องความเป็นอยู่ในจุฬาที่บูธกิจกรรมกลาง และเสวนาต่าง ๆ บนเวทีกลางรออยู่! \n 2. กิจกรรมของคณะ @ จัดที่คณะต่าง ๆ ปีนี้คณะต่าง ๆ ภายในจุฬาก็ได้จัดกิจกรรมที่คณะตนเอง รอให้น้อง ๆ จับจองที่นั่งเข้าร่วมอยู่!",
     },
     {
       index: "2",
-      question: "How can I register for CU Openhouse 2026?",
+      question: "ต้องลงทะเบียนหรือไม่?",
       answer:
-        "You can register for CU Openhouse 2026 through our official website. Registration details will be available closer to the event date.",
-    }
+        "ต้องลงทะเบียนเข้าร่วมงานทุกคน! โดยแบ่งการลงทะเบียนเป็น 2 ส่วน ตามการจัดงานเลย ได้แก่ \n1.ลงทะเบียนส่วนกลาง \n\t1.1. ลงทะเบียนผ่านเว็บไซต์กลาง cuopenhouse2026.com (เว็บไซต์นี้) \n\t1.2.ทุกคนต้องลงทะเบียน เพื่อให้ได้ QR Code ประจำตัว นำไปให้พี่ ๆ แสกนก่อนเข้างานทุกที่ เช่น ก่อนเข้างานที่ศาลาพระเกี้ยว, ก่อนเข้าร่วมงานที่คณะนิติศาสตร์ \n\t1.3. ไม่มีการจำกัดจำนวนเข้าร่วม \n\t1.4.สามารถลงได้เรื่อย ๆ ตั้งแต่วันนี้ จนถึงวันงาน! \n2. ลงทะเบียนเข้าร่วมกิจกรรม \n\t2.1. ติดตามได้ผ่าน ig ของคณะต่าง ๆ หรือ เลือกกิจกรรมคณะที่สนใจ และคลิกลิงก์ได้ ผ่านเว็บไซต์นี้ \n\t2.2. สามารถเลือกลงทะเบียนเข้าร่วมกิจกรรมของคณะที่สนใจ เช่น ห้องเรียนจำลอง, การแสดง, ห้องแล็ป \n\t2.3. สามารถเลือกลงกี่คณะก็ได้ แต่ต้องจัดสรรเวลาให้ไม่ซ้อนกัน",
+    },
+    {
+      index: "3",
+      question: "ใครสามารถเข้าร่วมได้บ้าง?",
+      answer:
+        "ทุกคนสามารถเข้าร่วมงานได้ เพียงแค่ลงทะเบียนผ่านเว็บไซต์นี้ แม้แต่ผู้ปกครองที่ต้องการเข้าร่วมพร้อมน้อง ๆ !",
+    },
+    {
+      index: "4",
+      question: "ต้องใส่ชุดอะไร?",
+      answer:
+        "สามารถเลือกใส่ชุดที่น้อง ๆ มั่นใจได้เลย! โดยอยากให้คำนึงถึงความสะดวกสบาย และสภาพอากาศในเดือนนั้น ๆ",
+    },
+    {
+      index: "5",
+      question: "มีที่จอดรถหรือไม่?",
+      answer:
+        "สามารถเลือกจอดได้ภายในจุฬา เช่น ตึกจามจุรี 9, อาคารรัฐศาสตร์ หรือรอบ ๆ จุฬา เช่น สามย่านมิตรทาวน์, block 28, ใต้หอพัก u-center, สยามสแควร์, mbk และอื่น ๆ",
+    },
   ];
 
   return (
@@ -51,7 +69,7 @@ export default function LandingSectionFour() {
               <AccordionContent
                 className={clsx(
                   "bg-main-beige px-4! py-2! rounded-b-[10px] font-normal text-xs leading-4.5 by-1",
-                  "overflow-hidden transition-all duration-300 ease-in-out",
+                  "overflow-hidden whitespace-pre-line transition-all duration-300 ease-in-out",
                   "data-[state=closed]:animate-accordion-up",
                   "data-[state=open]:animate-accordion-down"
                 )}
