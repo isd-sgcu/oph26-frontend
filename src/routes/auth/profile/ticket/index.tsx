e27bb2644b47649c83c6633051e838a545c5235c
yiwahpsp <patsaporn.kaewkong@gmail.com>
2026-03-17 23:53:04 +0700

    feat: lib getMyAttendee

--- src/routes/auth/profile/ticket/index.tsx
+++ src/routes/auth/profile/ticket/index.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/ui/button'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, notFound } from '@tanstack/react-router'
 import { useNavigate } from '@tanstack/react-router'
 import Ticket from '@/components/auth/profile/ticket'
 import { useTranslation } from 'react-i18next'
+import { useState, useEffect } from 'react'
+import { getMyAttendee, GetMyAttendeeResponse } from '@/services/attendee/attendee'
 
 export const Route = createFileRoute('/auth/profile/ticket/')({
   component: RouteComponent,
@@ -12,25 +14,47 @@ function RouteComponent() {
   const navigate = useNavigate()
   const { t } = useTranslation()
 
+  const [attendee, setAttendee] = useState<GetMyAttendeeResponse | null>(null)
+
+  useEffect(() => {
+    const fetchAttendee = async () => {
+      try {
+        const response = await getMyAttendee()
+        if (!response) {
+          return notFound()
+        }
+        setAttendee(response)
+      } catch (error) {
+        console.error('Error fetching attendee:', error)
+      }
+    };
+
+    fetchAttendee()
+  }, [])
+
   return (
-    <div className="relative h-full min-h-dvh w-full p-5">
+    <div className="relative p-5 w-full h-full min-h-dvh">
       <img
         src="/background/background-1.webp"
         alt=""
-        className="absolute inset-0 z-0 h-full w-full object-cover"
+        className="z-0 absolute inset-0 w-full h-full object-cover"
       />
-      <div className="relative z-10 flex flex-col items-center justify-center gap-5">
+      <div className="z-10 relative flex flex-col justify-center items-center gap-5">
         {/* --- Ticket Container --- */}
-        <Ticket
-          id="1234567890"
-          firstName="Phakpong"
-          lastName="Thaveepanya"
-          status={true}
-          dreamFaculties={['ENG', 'SCI', 'edu', 'MD']}
-        />
+        {
+          attendee && (
+            <Ticket
+              id={attendee.ticket_code}
+              firstName={attendee.firstname}
+              lastName={attendee.surname}
+              status={false}
+              dreamFaculties={attendee.interested_faculty}
+            />
+          )
+        }
 
         {/* --- Footer Buttons --- */}
-        <div className="flex w-full flex-wrap items-center justify-center gap-6 pb-5">
+        <div className="flex flex-wrap justify-center items-center gap-6 pb-5 w-full">
           <Button
             size={'lg'}
             className="bg-main-beige text-main-pink"
--- src/services/attendee/attendee.ts
+++ src/services/attendee/attendee.ts
@@ -18,13 +18,40 @@ export type CreateAttendeeRequest = {
   transportationMethod: string
 }
 
+export interface GetMyAttendeeResponse {
+  date_of_birth: string;
+  attendee_type: string;
+  certificate_name: string | null;
+  checked_in_at: string | null;
+  checkin_staff_id: string | null;
+  createdAt: string;
+  favorite_workshops: string[];
+  firstname: string;
+  id: string;
+  initial_first_interested_faculty: string;
+  interested_faculty: string[];
+  news_sources_other: string;
+  news_sources_selected: string[];
+  objective_other: string;
+  objective_selected: string[];
+  province: string;
+  district: string;
+  school_name: string;
+  study_level: string;
+  surname: string;
+  ticket_code: string;
+  updatedAt: string;
+  user_id: string;
+  transportation_method: string;
+}
+
 export const createAttendee = async (
   payload: CreateAttendeeRequest
 ): Promise<void> => {
   await Axios.post(`/attendees/`, payload)
 }
 
-export const getMyAttendee = async (): Promise<Attendee | null> => {
+export const getMyAttendee = async (): Promise<GetMyAttendeeResponse | null> => {
   try {
     const { data } = await Axios.get(`/attendees/me`)
     return data
